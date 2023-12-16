73ba4449348b61c5e75d60e6d36ec4f74d6628c6
Tay Kang Sheng <kangsheng@gmail.com>
2023-12-16 22:55:17 +0800

    update setup-ruby to use chruby

--- setup-scripts/setup-ruby.sh
+++ setup-scripts/setup-ruby.sh
@@ -1,8 +1,6 @@
 set -e
 set -x
 
-# this script install ruby using rvm. checkout https://rvm.io for more info
-
-brew install gpg2
-gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable
+brew update
+brew install chruby
+ruby-install ruby
